e1ed2839f578eecd46b8c96cba6a9827a1e7e4f0
yy6yy@live.cn <dongsiqi12>
2018-07-23 15:50:44 +0800

    新增功能

--- src/app/routes/cus-district/meter/emeter/emeter.component.ts
+++ src/app/routes/cus-district/meter/emeter/emeter.component.ts
@@ -7,36 +7,6 @@ import {
   SimpleTableData,
 } from '@delon/abc';
 
-const options = [{
-  value: 'zhejiang',
-  label: 'Zhejiang',
-  children: [{
-    value: 'hangzhou',
-    label: 'Hangzhou',
-    children: [{
-      value: 'xihu',
-      label: 'West Lake',
-      isLeaf: true
-    }]
-  }, {
-    value: 'ningbo',
-    label: 'Ningbo',
-    isLeaf: true
-  }]
-}, {
-  value: 'jiangsu',
-  label: 'Jiangsu',
-  children: [{
-    value: 'nanjing',
-    label: 'Nanjing',
-    children: [{
-      value: 'zhonghuamen',
-      label: 'Zhong Hua Men',
-      isLeaf: true
-    }]
-  }]
-}];
-
 @Component({
   selector: 'cus-emeter',
   templateUrl: './emeter.component.html',
@@ -59,6 +29,16 @@ export class EmeterComponent implements OnInit {
     this.i.classId = values[3]
   }
 
+  getBzStatusType(str: number): string {
+    const statusItem = this.bzstatus[str];
+    return statusItem.type;
+  }
+
+  getBzStatusText(str: number): string {
+    const statusItem = this.bzstatus[str];
+    return statusItem.text;
+  }
+
   getStatusType(str: number): string {
     const statusItem = this.status[str];
     return statusItem.type;
@@ -75,6 +55,11 @@ export class EmeterComponent implements OnInit {
   isConfirmLoading = false;
 
   loading = false;
+  bzstatus = [
+    {index: 0, text: '待开户', value: false, type: 'default', checked: false},
+    {index: 1, text: '已开户', value: false, type: 'success', checked: false},
+    {index: 2, text: '已注销', value: false, type: 'error', checked: false},
+  ];
   status = [
     {index: 0, text: '删除', value: false, type: 'error', checked: false},
     {index: 1, text: '正常', value: false, type: 'success', checked: false},
@@ -87,15 +72,25 @@ export class EmeterComponent implements OnInit {
     {title: '表号', index: 'no'},
     {title: '客户', index: 'cusinfoId.name'},
     {title: '办公室', index: 'classAllPath', format: (item: any) => this.getClassAllPathName(item)},
-    {title: '办公室', index: 'classId'},
-    {title: '状态', index: 'status', render: 'status'},
+    {title: '开户状态', index: 'bzstatus', render: 'bzstatus'},
+    {title: '记录状态', index: 'status', render: 'status'},
     {title: '更新时间', index: 'date', type: 'date'},
     {title: '备注', index: 'introduction'},
     {
       title: '操作',
       buttons: [
+        {
+          text: '开户',
+          click: (item: any) => this.showModal(item),
+          iif: (item: any) => item.status === 1 && item.bzstatus === 0
+        },
+        {
+          text: '注销',
+          click: (item: any) => this.showModal(item),
+          iif: (item: any) => item.status === 1 && item.bzstatus === 1
+        },
         {text: '编辑', click: (item: any) => this.showModal(item), iif: (item: any) => item.status === 1},
-        {text: '删除', type: 'del', click: (item: any) => this.delIteml(item), iif: (item: any) => item.status === 1}
+        {text: '删除', type: 'del', click: (item: any) => this.delIteml(item), iif: (item: any) => item.status === 1 && item.bzstatus === 0}
       ],
     },
   ];
@@ -103,13 +98,10 @@ export class EmeterComponent implements OnInit {
   expandForm = false;
 
   constructor(private http: _HttpClient, public msg: NzMessageService) {
-    console.log("9")
     this.getNodes();
   }
 
   getClassAllPathName(item: any) {
-    console.log(this.companyObject)
-    console.log(item.classAllPath[0])
     var companyId = item.classAllPath[0]
     var communityId = item.classAllPath[1]
     var floorId = item.classAllPath[2]
@@ -181,17 +173,17 @@ export class EmeterComponent implements OnInit {
                           floorObj.children.push(classObj)
                         }
                       }
-                      if(floorObj.children.length>0){
+                      if (floorObj.children.length > 0) {
                         communityObj.children.push(floorObj)
                       }
                     }
                   }
-                  if(communityObj.children.length>0){
+                  if (communityObj.children.length > 0) {
                     companyObj.children.push(communityObj)
                   }
                 }
               }
-              if (companyObj.children.length > 0){
+              if (companyObj.children.length > 0) {
                 ar.push(companyObj)
               }
             }
