3f40bc33640c26723429d73954b28ef7de9126d6
yy6yy@live.cn <dongsiqi12>
2018-07-23 14:56:45 +0800

    新增功能

--- src/app/routes/cus-district/meter/emeter/emeter.component.ts
+++ src/app/routes/cus-district/meter/emeter/emeter.component.ts
@@ -1,11 +1,41 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {NzMessageService} from 'ng-zorro-antd';
+import {NzCascaderComponent, NzMessageService} from 'ng-zorro-antd';
 import {_HttpClient} from '@delon/theme';
 import {
   SimpleTableComponent,
   SimpleTableColumn,
   SimpleTableData,
 } from '@delon/abc';
+
+const options = [{
+  value: 'zhejiang',
+  label: 'Zhejiang',
+  children: [{
+    value: 'hangzhou',
+    label: 'Hangzhou',
+    children: [{
+      value: 'xihu',
+      label: 'West Lake',
+      isLeaf: true
+    }]
+  }, {
+    value: 'ningbo',
+    label: 'Ningbo',
+    isLeaf: true
+  }]
+}, {
+  value: 'jiangsu',
+  label: 'Jiangsu',
+  children: [{
+    value: 'nanjing',
+    label: 'Nanjing',
+    children: [{
+      value: 'zhonghuamen',
+      label: 'Zhong Hua Men',
+      isLeaf: true
+    }]
+  }]
+}];
 
 @Component({
   selector: 'cus-emeter',
@@ -17,8 +47,17 @@ export class EmeterComponent implements OnInit {
   ps = 10;
   args: any = {};
 
+  companyObject: any = {};
+  communityObject: any = {};
+  floorObject: any = {};
+  classObject: any = {};
+
   listOfOption = [];
-  listOfTagOptions = [];
+  nzOptions = [];
+
+  public onChanges(values: any): void {
+    this.i.classId = values[3]
+  }
 
   getStatusType(str: number): string {
     const statusItem = this.status[str];
@@ -30,8 +69,8 @@ export class EmeterComponent implements OnInit {
     return statusItem.text;
   }
 
-  i:{cusinfoId?:any} = {};
-  cusinfoId:any = {};
+  i: any = {};
+  cusinfoId: any = {};
   isVisible = false;
   isConfirmLoading = false;
 
@@ -41,10 +80,13 @@ export class EmeterComponent implements OnInit {
     {index: 1, text: '正常', value: false, type: 'success', checked: false},
   ];
   @ViewChild('st') st: SimpleTableComponent;
+  @ViewChild('cascader') cascader: NzCascaderComponent;
+
   columns: SimpleTableColumn[] = [
     {title: '', index: 'key', type: 'checkbox'},
-    {title: '表编号', index: 'no'},
+    {title: '表号', index: 'no'},
     {title: '客户', index: 'cusinfoId.name'},
+    {title: '办公室', index: 'classAllPath', format: (item: any) => this.getClassAllPathName(item)},
     {title: '办公室', index: 'classId'},
     {title: '状态', index: 'status', render: 'status'},
     {title: '更新时间', index: 'date', type: 'date'},
@@ -61,12 +103,27 @@ export class EmeterComponent implements OnInit {
   expandForm = false;
 
   constructor(private http: _HttpClient, public msg: NzMessageService) {
+    console.log("9")
+    this.getNodes();
   }
 
-  cusinfoIdChange(data){
+  getClassAllPathName(item: any) {
+    console.log(this.companyObject)
+    console.log(item.classAllPath[0])
+    var companyId = item.classAllPath[0]
+    var communityId = item.classAllPath[1]
+    var floorId = item.classAllPath[2]
+    var classId = item.classAllPath[3]
+    return this.companyObject[companyId] + ","
+      + this.communityObject[communityId] + ","
+      + this.floorObject[floorId] + ","
+      + this.classObject[classId]
+  }
+
+
+  cusinfoIdChange(data) {
     this.i.cusinfoId._id = data;
   }
-
 
   getData() {
     this.st.pi = 1;
@@ -74,14 +131,91 @@ export class EmeterComponent implements OnInit {
   }
 
   ngOnInit() {
-    const children = [];
-    children.push({label:"iii",value:"L7bqEd-ay"})
-    children.push({label:"lll",value:"ej83gUeio"})
-    for (let i = 10; i < 36; i++) {
-      children.push({ label: i.toString(36) + i, value: i.toString(36) + i });
-    }
+  }
 
-    this.listOfOption = children;
+  getNodes(): any {
+    var ar = []
+    this.http
+      .post('/api/company/getCompanyInfoTree', {})
+      .subscribe(
+        (obj: any) => {
+          if (obj.state == "success") {
+            var companyList = obj.companyList;
+            var communityList = obj.communityList;
+            var floorList = obj.floorList;
+            var classList = obj.classList;
+
+            for (var i in companyList) {
+              this.companyObject[companyList[i]._id] = companyList[i].name
+              var companyObj = {
+                label: companyList[i].name,
+                value: companyList[i]._id,
+                children: []
+              }
+
+              for (var j in communityList) {
+                this.communityObject[communityList[j]._id] = communityList[j].name
+                if (companyList[i]._id === communityList[j].companyId) {
+                  var communityObj = {
+                    label: communityList[j].name,
+                    value: communityList[j]._id,
+                    children: []
+                  }
+
+                  for (var k in floorList) {
+                    this.floorObject[floorList[k]._id] = floorList[k].name
+                    if (communityList[j]._id === floorList[k].communityId) {
+                      var floorObj = {
+                        label: floorList[k].name,
+                        value: floorList[k]._id,
+                        children: []
+                      }
+                      for (var o in classList) {
+                        this.classObject[classList[o]._id] = classList[o].name
+                        if (floorList[k]._id === classList[o].floorId) {
+                          var classObj = {
+                            label: classList[o].name,
+                            value: classList[o]._id,
+                            isLeaf: true,
+                          }
+                          floorObj.children.push(classObj)
+                        }
+                      }
+                      if(floorObj.children.length>0){
+                        communityObj.children.push(floorObj)
+                      }
+                    }
+                  }
+                  if(communityObj.children.length>0){
+                    companyObj.children.push(communityObj)
+                  }
+                }
+              }
+              if (companyObj.children.length > 0){
+                ar.push(companyObj)
+              }
+            }
+            this.nzOptions = ar;
+          } else {
+            this.msg.error(obj.message)
+          }
+        }
+      )
+
+    this.http
+      .post('/api/cusinfo/getCusinfoInfos?ps=999&pi=1', {})
+      .subscribe(
+        (obj: any) => {
+          if (obj.state == "success") {
+            this.listOfOption = []
+            for (var i in obj.companyInfos) {
+              this.listOfOption.push({label: obj.companyInfos[i].name, value: obj.companyInfos[i]._id})
+            }
+          } else {
+            this.msg.error(obj.message)
+          }
+        }
+      )
   }
 
   checkboxChange(list: SimpleTableData[]) {
@@ -121,10 +255,10 @@ export class EmeterComponent implements OnInit {
       this.i = item;
       this.cusinfoId = this.i.cusinfoId
     } else {
-      this.i = {cusinfoId:{}}
+      this.i = {cusinfoId: {}, classAllPath: []}
+      this.cusinfoId = {}
+      // this.cascader.in
     }
-
-    console.log("i:", this.i)
     this.isVisible = true;
   }
 
@@ -137,11 +271,9 @@ export class EmeterComponent implements OnInit {
           this.isConfirmLoading = false;
           this.isVisible = false;
           this.st.reload()
-          console.log(obj)
         } else {
           this.isConfirmLoading = false;
           this.msg.error(obj.message)
-          console.log(obj)
         }
       }
     )
